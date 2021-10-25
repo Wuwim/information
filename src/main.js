db1c98cae145dd3e9a512a3ddb0ee07702669681
Wuwim <2683389760@qq.com>
2021-10-25 14:53:00 +0800

    14.52

--- src/main.js
+++ src/main.js
@@ -2,13 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import Vant from 'vant';
-import 'vant/lib/index.css';
+import { Form } from 'vant';
+import { Field } from 'vant';
+import { Picker } from 'vant';
+import { Popup } from 'vant';
+import { Calendar } from 'vant';
+import { Uploader } from 'vant';
+import { Button } from 'vant';
+import { Area } from 'vant';
+import { DatetimePicker } from 'vant';
 
-Vue.use(Vant);
 
-
-import 'mint-ui/lib/style.css'
+Vue.use(Form);
+Vue.use(Field);
+Vue.use(Picker);
+Vue.use(Popup);
+Vue.use(Calendar);
+Vue.use(Uploader);
+Vue.use(Button);
+Vue.use(Area);
+Vue.use(DatetimePicker);
 import 'amfe-flexible';
 
 import VueDirectiveImagePreviewer from 'vue-directive-image-previewer'
